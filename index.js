92bf112b1a0018598e451ddeb062eb6bf575f13f
Durodola Abdulhameed <138580367+olaife219@users.noreply.github.com>
2024-12-18 06:45:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
-const db = require('./db')
+// const MySQLStore = require('express-mysql-session')(session);
+// const db = require('./db')
 const route = require('./router');
 require('dotenv').config();
 const cors = require("cors");
@@ -18,32 +18,32 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const sessionStore = new MySQLStore({}, db);
+// const sessionStore = new MySQLStore({}, db);
 
-app.use(session({
-    key: 'user_cookies',
-    secret: 'your-secret-key',
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false }
-  }));
+// app.use(session({
+//     key: 'user_cookies',
+//     secret: 'your-secret-key',
+//     store: sessionStore,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { secure: false }
+//   }));
 
 
   app.get('/', (req, res) => {
     console.log(req.session);
     console.log(req.session.id);
-    res.json(req.session.user);    
+    res.send("Hello, wellcome to backend");    
   })
 
 // logic to get all users
-app.get('/api/users', (req, res) => {
-    const sql = 'SELECT id, name, email FROM users';
-    db.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json(results);
-    });
-});
+// app.get('/api/users', (req, res) => {
+//     const sql = 'SELECT id, name, email FROM users';
+//     db.query(sql, (err, results) => {
+//         if (err) throw err;
+//         res.json(results);
+//     });
+// });
 
 app.listen(5000, () => {
     console.log('server is listening on http://localhost:5000');
